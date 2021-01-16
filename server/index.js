f8ce164dcd3281a00163c4486fa51267ad77a4ae
brivu <emailbrian.vu@gmail.com>
2021-01-15 22:17:20 -1000

    Completed CSS

--- server/index.js
+++ server/index.js
@@ -14,20 +14,29 @@ app.use(compression());
 
 app.get('/images', async (req, res)=>{
   //let bundle = await axios.get('http://localhost:3006/items/1/bundle.js');
-  let bundle = await axios.get('http://13.52.213.118:3006/items/1/bundle.js');
+  let bundle = await axios.get('http://13.52.213.118:3006/items/1/bundle.js')
+  .catch((err)=>{
+    console.log('ERROR: ', err);
+  });
   res.send(bundle.data);
 });
 
 app.get('/item/:itemId/images', async (req, res)=>{
   let item_id = req.params.itemId;
  //let image_urls = await axios.get(`http://localhost:3006/item/${item_id}/images`);
-  let image_urls = await axios.get(`http://13.52.213.118:3006/item/${item_id}/images`);
+  let image_urls = await axios.get(`http://13.52.213.118:3006/item/${item_id}/images`)
+  .catch((err)=>{
+    console.log('ERROR: ', err);
+  });
   res.send(image_urls.data);
 });
 
 app.get('/item/images', async (req, res)=>{
  //let image_urls = await axios.get('http://localhost:3006/item/images');
-  let image_urls = await axios.get('http://13.52.213.118:3006/item/images');
+  let image_urls = await axios.get('http://13.52.213.118:3006/item/images')
+  .catch((err)=>{
+    console.log('ERROR: ', err);
+  });
   res.send(image_urls.data);
 })
 
@@ -35,7 +44,10 @@ app.get('/item/images', async (req, res)=>{
 
 app.get('/seller', async (req, res)=>{
   //let bundle = await axios.get('http://localhost:3005/items/1/bundle.js');
-  let bundle = await axios.get('http://3.21.248.149:3005/items/1/bundle.js');
+  let bundle = await axios.get('http://3.21.248.149:3005/items/1/bundle.js')
+  .catch((err)=>{
+    console.log('ERROR: ', err);
+  });
   res.send(bundle.data);
 });
 
@@ -43,6 +55,9 @@ app.get('/items/:item_id/seller', async (req, res) => {
   let item_id = req.params.item_id;
   //let seller_data = await axios.get(`http://localhost:3005/items/${item_id}/seller`);
   let seller_data = await axios.get(`http://3.21.248.149:3005/items/${item_id}/seller`)
+  .catch((err)=>{
+    console.log('ERROR: ', err);
+  });
   res.send(seller_data.data);
 })
 
@@ -51,21 +66,30 @@ app.get('/items/:item_id/seller', async (req, res) => {
 // //SHOPPING SERVICE ENDPOINTS
 
 app.get('/shopping', async (req, res)=>{
-  //let bundle = await axios.get('http://localhost:3004/items/1/bundle.js');
-  let bundle = await axios.get('http://18.222.223.190:3004/items/1/bundle.js');
+  //let bundle = await axios.get('http://localhost:3004/items/1/bundle.js')
+  let bundle = await axios.get('http://18.222.223.190:3004/items/1/bundle.js')
+  .catch((err)=>{
+    console.log('ERROR: ', err);
+  });
   res.send(bundle.data);
 });
 
 app.get('/shopping/items', async (req, res) => {
   //let shopping_data = await axios.get('http://localhost:3004/shopping/items');
-  let shopping_data = await axios.get('http://18.222.223.190:3004/shopping/items');
+  let shopping_data = await axios.get('http://18.222.223.190:3004/shopping/items')
+  .catch((err)=>{
+    console.log('ERROR: ', err);
+  });
   res.send(shopping_data.data);
 });
 
 app.get('/shopping/items/:item_id', async (req, res) => {
   let item_id = req.params.item_id;
   //let shopping_data = await axios.get(`http://localhost:3004/shopping/items/${item_id}`);
-  let shopping_data = await axios.get(`http://18.222.223.190:3004/shopping/items/${item_id}`);
+  let shopping_data = await axios.get(`http://18.222.223.190:3004/shopping/items/${item_id}`)
+  .catch((err)=>{
+    console.log('ERROR: ', err);
+  });
   res.send(shopping_data.data);
 });
 
@@ -73,14 +97,20 @@ app.get('/shopping/items/:item_id', async (req, res) => {
 
 app.get('/reviews', async (req, res)=>{
   //let bundle = await axios.get('http://localhost:3002/items/1/bundle.js');
-  let bundle = await axios.get('http://54.151.123.24:3002/items/1/bundle.js');
+  let bundle = await axios.get('http://54.151.123.24:3002/items/1/bundle.js')
+  .catch((err)=>{
+    console.log('ERROR: ', err);
+  });
   res.send(bundle.data);
 });
 
 app.get('/api/items/:itemId/reviews', async (req, res) => {
   let item_id = req.params.itemId;
   //let reviews_data = await axios.get(`http://localhost:3002/api/items/${item_id}/reviews`);
-  let reviews_data = await axios.get(`http://54.151.123.24:3002/api/items/${item_id}/reviews`);
+  let reviews_data = await axios.get(`http://54.151.123.24:3002/api/items/${item_id}/reviews`)
+  .catch((err)=>{
+    console.log('ERROR: ', err);
+  });
   res.send(reviews_data.data);
 });
 
